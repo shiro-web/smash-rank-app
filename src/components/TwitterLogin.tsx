c96181f82b1670137d9dd762cb8474bc106aee5e
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-01-31 20:49:54 +0900

    Twitterログイン機能のコンポーネントを新しく作成

---
+++ src/components/TwitterLogin.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { auth } from '@/firebase';
+import {TwitterAuthProvider, signInWithPopup} from 'firebase/auth';
+import React from 'react';
+import classes from "./TwitterLogin.module.scss";
+import { useRouter } from 'next/navigation';
+
+
+const TwitterLogin = () => {
+    const router = useRouter();
+    const twitterprovider = new TwitterAuthProvider();
+
+    const twitterLogin = () => {
+        signInWithPopup(auth,twitterprovider).then((result) => {
+            const user = result.user
+            if(user){
+              router.push(`/mypage/${user.uid}`)
+            }
+    })}
+
+  return (
+    <div className={classes.twitterLoginWrapper}>
+        <button onClick={twitterLogin} className={classes.twitterLogin}>Xでログイン</button>
+    </div>
+  )
+}
+
+export default TwitterLogin
