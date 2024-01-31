b06987f6822d4217bbcb1238ef951b2cdbea4b56
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-01-31 17:28:02 +0900

    authページを非表示にした

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,18 +1,18 @@
-"use client";
+// "use client";
 
-import TwitterLogin from '@/components/HeaderTwitterLogin';
-import { auth } from '@/firebase';
-import { GoogleAuthProvider, TwitterAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+// import TwitterLogin from '@/components/HeaderTwitterLogin';
+// import { auth } from '@/firebase';
+// import { GoogleAuthProvider, TwitterAuthProvider, signInWithPopup } from 'firebase/auth';
+// import React from 'react';
 
-const page = () => {
+// const page = () => {
 
-  return (
-    <div>
-        <button onClick={googleLogin}>Googleログイン</button>
-        <TwitterLogin/>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//         <button onClick={googleLogin}>Googleログイン</button>
+//         <TwitterLogin/>
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
