23f982e28dfaa124606d5fea9f091350851cd622
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-01 23:35:13 +0900

    サイドsigninwithpopupに変更

--- src/components/TwitterLogin.tsx
+++ src/components/TwitterLogin.tsx
@@ -12,7 +12,7 @@ const TwitterLogin = () => {
     const twitterprovider = new TwitterAuthProvider();
 
     const twitterLogin = () => {
-        signInWithRedirect(auth,twitterprovider).then((result) => {
+        signInWithPopup(auth,twitterprovider).then((result) => {
             const user = result.user
             if(user){
               router.push(`/mypage/${user.uid}`)
