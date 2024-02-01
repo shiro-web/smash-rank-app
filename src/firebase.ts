d1f62cb9557704b24b0a2c56732d7ad373d1dd95
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-01 13:04:59 +0900

    アナリティクスのID表示

--- src/firebase.ts
+++ src/firebase.ts
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-// export const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
