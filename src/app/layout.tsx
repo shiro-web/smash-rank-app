dbe3bdafbf9e46c23402146d8074851c9d2b968e
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-02 15:49:30 +0900

    googleアドセンス追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "the-new-css-reset/css/reset.css";
 import { AppProvider } from "@/context/AppContext";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "スマブラ世界戦闘力ランキング",
@@ -17,6 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+     <head>
+        <Script
+          id="Absence-banner"
+          async
+          strategy="lazyOnload"
+          src={"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3254973128088123"}
+          crossOrigin="anonymous"
+        />
+      </head>
       <AppProvider>
         <body>
           <Header/>
