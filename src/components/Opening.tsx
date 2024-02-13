8ae45c499617f9559139bbb09913c5aafa2b6dfe
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-13 19:25:25 +0900

    細かい修正

--- src/components/Opening.tsx
+++ src/components/Opening.tsx
@@ -2,24 +2,22 @@
 
 import React, { useEffect, useState } from 'react';
 import classes from "./Opening.module.scss";
-import { useRouter } from 'next/navigation';
 
 const Opening = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
-        //セッションストレージで初回訪問かどうかを判定
         const handleReload = () => {
           setIsLoading(true);
           setTimeout(() => {
             setIsLoading(false);
           }, 2000);
-          return
         };
         return () => {
           handleReload()
         };
       }, [])
+      
   return (
     <div>
       {isLoading ? (<div className={classes.openingWrapper} id="fadeInBox">
