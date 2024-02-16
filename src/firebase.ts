d64d3866490fe6d06df39e81e7e590c3e7203af9
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-16 17:55:12 +0900

    test

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,6 +13,7 @@ const firebaseConfig = {
   // measurementId: "G-ZY8ER3CZBB"
 };
 
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
