4867496eb9d380f4098504c6d8efcb3698a0d8a7
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-16 17:40:18 +0900

    🚑 firebase初期化

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyDxZVpe5O2CKhmCD9Ob4e78M27l-LwbqmI",
-  authDomain:"smash-rank-app-52c1e.firebaseapp.com",
-  projectId:"smash-rank-app-52c1e",
+  apiKey: "AIzaSyDxZVpe5O2CKhmCD9Ob4e78M27l-LwbqmI",
+  authDomain: "smash-rank-app-52c1e.firebaseapp.com",
+  projectId: "smash-rank-app-52c1e",
   storageBucket: "smash-rank-app-52c1e.appspot.com",
-  messagingSenderId:"49555915234",
-  appId:"1:49555915234:web:7b925847f5eb8b98edb9bb" ,
-  // measurementId:"G-ZY8ER3CZBB"
+  messagingSenderId: "49555915234",
+  appId: "1:49555915234:web:7b925847f5eb8b98edb9bb",
+  // measurementId: "G-ZY8ER3CZBB"
 };
 
 const app = initializeApp(firebaseConfig);
