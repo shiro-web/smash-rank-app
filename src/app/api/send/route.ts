ed2c5cce92bccf4fdb143efa0ca6ad4cf6170687
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-02-19 17:43:17 +0900

    test

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from '@/components/email-template';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_RCvPwyfs_Jnx5xQUuZZ5jih4zoq8RBRwA");
 
 export async function POST(request:Request) {
     const {content,email} = await request.json();
