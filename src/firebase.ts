6b0fa9c305f4d50e9351a88a1c71fd970645fef5
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-03-28 22:52:15 +0900

    analytics修正

--- src/firebase.ts
+++ src/firebase.ts
@@ -16,5 +16,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+export const analytics =
+  process.env.NODE_ENV === 'production' && typeof window !== 'undefined'
+    ? getAnalytics(app)
+    : undefined
 
