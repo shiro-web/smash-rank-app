a277cd9f1ddde7e2f8c462353e6e9e336d3f0c3f
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-04-17 17:16:01 +0900

    ページ準備中

---
+++ src/app/fighters/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React, { useEffect } from 'react';
+import classes from "./page.module.scss";
+
+type Inputs = {
+    email:string;
+    content: string;
+  }
+
+const Fiters = () => {
+
+
+  return (
+    <div className={classes.container}>
+      <p className={classes.text}>このページは準備中です！<br />もう少々お待ちください<br />( ┓ ›ﾟдﾟ‹)┓｛ごめんなさぁぁぁいいいぃぃぃ!!)</p>
+    </div>
+  )
+}
+
+export default Fiters
